f13aa5bbf9d54ce05c41486ce390e512a957ba45
Vasilina-Zl <terisharkis@gmail.com>
2022-02-13 14:46:58 +0200

    first

---
+++ Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2._3
+{
+	
+		public class Employee
+		{
+			string name;
+			string lastName;
+			string position;
+			int experience;
+
+			public Employee(string name, string lastName, string position, int experience)
+			{
+			this.name = name;
+			this.lastName = lastName;
+			this.position = position;
+			this.experience = experience;
+			}
+			public void Deconstruct(out string eName, out string eLastName, out string ePosition, out int eExperience)
+			{
+			eName = name;
+			eLastName = lastName;	
+			ePosition = position;
+			eExperience = experience;
+			}
+
+
+			public int Salary()
+			{
+				int salary;
+				if (position == "Boss")
+				{
+					salary = 10000;
+				return salary;
+				}
+				else if (position == "Worker")
+				{
+				salary = 7000;
+				return salary;
+				}
+				else
+				{
+					salary = 3000;
+				return salary;
+				}
+			}
+			public double Oklad() 
+			{
+				int salary, exp, finalSalary;
+				salary = Salary();
+				exp = experience;
+				finalSalary = salary * exp;
+				return finalSalary;
+			}
+
+			public double Tax() 
+			{
+				double tax, procent, fs;
+				fs = Oklad();
+				procent = 0.18;
+				tax = fs * procent;
+				return tax;
+			}
+
+		}
+	}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2._3
+{
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+			Employee firstEmp = new Employee("Lina", "Zlenko", "Boss", 3);
+
+			
+			(string name, string lastName, string position, int experience) = firstEmp;
+			double oklad = firstEmp.Oklad();
+			double tax = firstEmp.Tax();
+			Console.WriteLine($"Iм'я: {name}; Прiзвище: {lastName}; Посада: {position}; Оклад: {oklad}; Податковий збiр: {tax}");
+			
+			
+			Employee secondEmp = new Employee("Vasia", "Pupkin", "Worker", 4);
+
+
+			(string sname, string slastName, string sposition, int sexperience) = secondEmp;
+			double soklad = secondEmp.Oklad();
+			double stax = secondEmp.Tax();
+			Console.WriteLine($"Iм'я: {sname}; Прiзвище: {slastName}; Посада: {sposition}; Оклад: {soklad}; Податковий збiр: {stax}");
+			Console.ReadKey();
+
+		}
+	}
+}
